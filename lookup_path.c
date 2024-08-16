565b3815bef129da21c6d225edf34a099be010ff
Clement Callejon <9397@holbertonstudents.com>
2024-08-17 00:32:19 +0200

    Renamed file to match function name

---
+++ lookup_path.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * lookup_path - Searches paths in PATH env variable to find binary location.
+ * @func: function to lookup the binary file for.
+ *
+ * Return: void.
+ */
+void lookup_path(const char *func)
+{
+	
+}
