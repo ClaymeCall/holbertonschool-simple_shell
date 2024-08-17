4109b4bf7868929e0a0689f6030e7e53632beb4b
Clement Callejon <9397@holbertonstudents.com>
2024-08-17 15:09:06 +0200

    First working version of lookup_path function, with associated unit testing main function

--- lookup_path.c
+++ lookup_path.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 
@@ -7,13 +8,13 @@ extern char **environ;
 /**
  * get_path_env - Searches for the PATH environment variable.
  *
- *
+ * Return: Double pointer to the PATH environment variable
  */
 char *get_path_env(void)
 {
 	char *path = NULL;
 	int i = 0;
-
+	
 	while (environ[i] != NULL)
 	{
 		if (strncmp(environ[i], "PATH=", 5) == 0)
@@ -24,7 +25,7 @@ char *get_path_env(void)
 		i++;
 	}
 
-	return path;
+	return (path);
 }
 /**
  * lookup_path - Searches paths in PATH env variable to find binary location.
@@ -36,6 +37,8 @@ char *lookup_path(const char *func)
 {
 	char *path = get_path_env();
 	char *token = NULL;
+	char *full_path = NULL;
+	size_t len_token, len_func;
 
 	if (path == NULL)
 	{
@@ -43,12 +46,30 @@ char *lookup_path(const char *func)
 		return (NULL);
 	}
 
+	token = strtok(path, ":");
 	while (token != NULL)
 	{
+		len_token = strlen(token);
+		len_func = strlen(func);
+
+		/* Allocate memory for full_path (token + "/" + func + '\0') */
+		full_path = malloc(len_token + 1 + len_func + 1);
+		if (full_path == NULL)
+			return (NULL);
+
+		/* Constructing the full path */
+		strcpy(full_path, token);
+		strcat(full_path, "/");
+		strcat(full_path, func);
+
+		/* Return path if it is executable */
+		if (access(full_path, X_OK) == 0)
+			return (full_path);
+
+		/* Free memory if path is not executable */
+		free(full_path);
 		token = strtok(NULL, ":");
-		if (access(strcat(token, func), X_OK))
-			break;
 	}
 
-	return (strcat(token, func));
+	return (NULL);
 }
