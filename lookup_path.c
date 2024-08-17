08a5efc9a1cb2930501f85cb6058f23f5189f027
Clement Callejon <9397@holbertonstudents.com>
2024-08-17 14:08:05 +0200

    Added first version of lookup_path function

--- lookup_path.c
+++ lookup_path.c
@@ -1,12 +1,54 @@
 #include "main.h"
+#include <unistd.h>
+#include <string.h>
 
+extern char **environ;
+
+/**
+ * get_path_env - Searches for the PATH environment variable.
+ *
+ *
+ */
+char *get_path_env(void)
+{
+	char *path = NULL;
+	int i = 0;
+
+	while (environ[i] != NULL)
+	{
+		if (strncmp(environ[i], "PATH=", 5) == 0)
+		{
+			path = environ[i] + 5;
+			break;
+		}
+		i++;
+	}
+
+	return path;
+}
 /**
  * lookup_path - Searches paths in PATH env variable to find binary location.
  * @func: function to lookup the binary file for.
  *
  * Return: void.
  */
-void lookup_path(const char *func)
+char *lookup_path(const char *func)
 {
-	
+	char *path = get_path_env();
+	char *token = NULL;
+
+	if (path == NULL)
+	{
+		printf("PATH not found\n");
+		return (NULL);
+	}
+
+	while (token != NULL)
+	{
+		token = strtok(NULL, ":");
+		if (access(strcat(token, func), X_OK))
+			break;
+	}
+
+	return (strcat(token, func));
 }
